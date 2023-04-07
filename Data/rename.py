ae5a51fa2fd4068dc60bb1e6cf67dd7313fad5d9
Ahan <64554828+AhanR@users.noreply.github.com>
2023-04-07 12:23:58 +0530

    remade imagedata for LST, LULC, NDVI

--- Data/rename.py
+++ Data/rename.py
@@ -5,8 +5,16 @@ def rename(folder = "Vandalur_LST"):
     mapSegments = glob.glob(folder+"/*.png")
     print("found "+str(len(mapSegments))+" images to rename")
     for file in mapSegments:
-        newname = "\\".join(file.split("\\")[:-1])+ "\\" +format(int(file.split("\\")[-1][:-4]),'04d') + ".png"
-        print(newname)
+        imnum = (int(file.split("\\")[-1].split("-")[1])-1)*20 + (int(file.split("\\")[-1].split("-")[3][:-4])-1)
+        newname = "\\".join(file.split("\\")[:-1])+ "\\" + format(imnum,'04d') + ".png"
+        # print(newname)
         os.rename(file, newname)
+    print("renaming complete")
 
-rename()
+# def recount(folder="Vandalur_LST"):
+#     mapSegments = glob.glob(folder+"/*.png")
+#     print("Found "+str(len(mapSegments)) + " images")
+#     for i in range(1,len(mapSegments)+1):
+#         if format(i, '04d') + ".png" in 
+
+rename("Vandalur_NDVI")
